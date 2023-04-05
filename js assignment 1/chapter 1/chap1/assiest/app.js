55b4a49628126c5fede28dc6bb2d08412fd833c1
mehak tariq <129633720+mehaktariq28@users.noreply.github.com>
2023-04-06 01:42:11 +0500

    Add files via upload

---
+++ js assignment 1/chapter 1/chap1/assiest/app.js
@@ -0,0 +1,15 @@
+Question_1:
+ alert("Hello How Are You.......!");
+
+ Question_2:
+ alert("Error! Please Enter a Valid password.....");
+
+ Question_3:
+ alert("Welcome to Java script Land Happy Coding!");
+
+ Question_4:
+ alert("Welcome to Java script Land.......");
+alert("Happy Coding!  Prevent this  page from creating  additional dialogs.");
+
+ Question_5:
+alert("Hello... I can run JS through my web browser's console");
